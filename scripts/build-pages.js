42583e0d76ce828f7f14653c869c89719283ab09
mitchellmos <mitchellmos@hotmail.nl>
2025-09-18 14:32:19 +0200

    Add favicon

--- scripts/build-pages.js
+++ scripts/build-pages.js
@@ -18,6 +18,7 @@ const baseTemplate = `<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{TITLE}}</title>
+    <link rel="icon" type="image/svg+xml" href="favicon.svg">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
