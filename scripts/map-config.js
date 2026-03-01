03d0de280021593ca30211f2fb9cc4f4d1d0a457
mitchellmos <mitchellmos@hotmail.nl>
2026-03-01 22:25:01 +0100

    Add 153/154, remove 146/147

--- scripts/map-config.js
+++ scripts/map-config.js
@@ -20,21 +20,17 @@ const MAP_TYPES = [
 
 // World mapping: internal name -> display name
 const WORLD_MAPPING = {
-
-
-
-
-    'en146': 'World 146', 
-    'en147': 'World 147',
     'en148': 'World 148',
     'en149': 'World 149',
     'en150': 'World 150',
     'en151': 'World 151',
-    'en152': 'World 152'
+    'en152': 'World 152',
+    'en153': 'World 153',
+    'en154': 'World 154'
 };
 
 // List of active worlds (internal names) - add new worlds here
-const WORLDS = ['en146', 'en147', 'en148', 'en149', 'en150', 'en151', 'en152'];
+const WORLDS = ['en148', 'en149', 'en150', 'en151', 'en152', 'en153', 'en154'];
 
 // Export for use in other files
 if (typeof module !== 'undefined' && module.exports) {
